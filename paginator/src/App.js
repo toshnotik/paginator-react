8e39472f0c3ed3e1d17adf98ad39e98b812709c4
toshnotik <toshnotik666@mail.ru>
2023-08-14 12:25:28 +0300

    okk

---
+++ paginator/src/App.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+import Countries from './components/Countries';
+import Pagination from './components/Pagination';
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [countriesPerPage] = useState(10)
+
+  useEffect(() => {
+    const getCounries = async ()=> {
+      setLoading(true)
+      const res = await axios.get('https://restcountries.com/v2/all')
+      setCountries(res.data)
+      setLoading(false)
+    }
+
+    getCounries()
+  }, [])
+
+  const lastCountryIndex = currentPage * countriesPerPage
+  const firstCoutryIndex = lastCountryIndex - countriesPerPage
+  const currentCountry = countries.slice(firstCoutryIndex, lastCountryIndex)
+
+  const paginate = pageNumber => setCurrentPage(pageNumber)
+  const nextPage = () => setCurrentPage(prev => prev + 1)
+  const prevPage = () => setCurrentPage(prev => prev - 1)
+
+  return (
+    <div className='conteiner mt-5'>
+      <h1 className='text-primary'>Страны</h1>
+      <Countries countries={currentCountry} loading={loading} />
+      <Pagination 
+        countriesPerPage={countriesPerPage}
+        totalCountries={countries.length}
+        paginate={paginate}
+      />
+
+      <button className='btn btn-primary' onClick={prevPage}>Предыдущая</button>
+      <button className='btn btn-primary ms-2' onClick={nextPage}>Следующая</button>
+    </div>
+  );
+}
+
+export default App;
